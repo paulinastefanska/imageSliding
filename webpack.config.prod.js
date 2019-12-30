e90866426dd07b8d1251479a166efe09c055122d
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-12-30 19:27:33 +0100

    Add webpack config

---
+++ webpack.config.prod.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: "./js/app.js"
+  },
+  output: {
+    filename: "js/[name]-[contenthash].js",
+    path: path.resolve(__dirname, "build")
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: "raw-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name]-[contenthash].css"
+    }),
+    new CopyPlugin([
+      {
+        from: "public/assets",
+        to: "images"
+      }
+    ])
+  ]
+};
